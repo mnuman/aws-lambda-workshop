8ab3a600861c29d4dd76f6f1bd6005133b29d891
Milco Numan <milco.numan@gmail.com>
2018-07-11 13:57:06 +0000

    Version getting/putting object using plain old callbacks

--- S3ImageManipulator/S3ImageManipulator/index.js
+++ S3ImageManipulator/S3ImageManipulator/index.js
@@ -3,5 +3,34 @@ const util = require('util');
 
 exports.handler = (event, context, callback) => {
     console.log(util.inspect(event, { showHidden: true, depth: null }));
-    callback();
+    const S3 = new AWS.S3();
+    const bucket = event.Records[0].s3.bucket.name;
+    const filekey = event.Records[0].s3.object.key;
+
+    var params = {
+      Bucket: bucket,
+      Key: filekey
+     };    
+
+    S3.getObject(params, (err, data) => {
+        if (err) {
+            console.log(err);
+            callback(err);
+        } else {
+            console.log("File size: " + data.ContentLength);
+            params.Key = 'sepia/' + params.Key.split('/')[1];
+
+            // for now: copy data ...
+            params.Body = data.Body;
+            // Simply putobject in sepia folder
+            S3.putObject(params, (err, data) => {
+                if (err) { 
+                    console.log(err);
+                    callback(err);
+                } else {
+                    callback();
+                }
+            } );
+        };
+    });
 };
